e6baff20e7986cbe30d5ff91d7b2549737bdcc8d
Fadi-Jawhar <fadi.jawhr@hotmail.com>
2024-12-03 17:55:12 +0100

    add questions.js

---
+++ src/questions.js
@@ -0,0 +1,23 @@
+export const questions = [
+    {
+        id: 1,
+        type: 'multiple',
+        question: 'Vilket år grundades Netflix?',
+        options: ['1997', '2004', '2009', '2012'],
+        correctAnswer: ['1997']
+    },
+    {
+        id: 2,
+        type: 'boolean',
+        question: 'Mbabane är huvudstaden i Swaziland.',
+        options: ['Sant', 'Falskt'],
+        correctAnswer: ['Sant']
+    },
+    {
+        id: 3,
+        type: 'boolean',
+        question: 'CSS står för Castrating Style Sheets.',
+        options: ['Sant', 'Falskt'],
+        correctAnswer: ['Falskt']
+    }
+];
