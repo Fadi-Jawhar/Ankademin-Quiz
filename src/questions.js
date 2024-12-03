2b20bf517fae36a39b35bd01ab9a15f1d1f1edb0
Fadi-Jawhar <fadi.jawhr@hotmail.com>
2024-12-03 18:02:59 +0100

    add more questions

--- src/questions.js
+++ src/questions.js
@@ -19,5 +19,54 @@ export const questions = [
         question: 'CSS står för Castrating Style Sheets.',
         options: ['Sant', 'Falskt'],
         correctAnswer: ['Falskt']
+    },
+    {
+        id: 4,
+        type: 'multiple',
+        question: 'Vad heter caféet i komediserien Vänner?',
+        options: ['Perk City', 'City Central', 'Central Perk', 'Park Central'],
+        correctAnswer: ['Central Perk']
+    },
+    {
+        id: 5,
+        type: 'multiple',
+        question: 'Vilken HTML-tag används för att skapa en länk?',
+        options: ['< link >', '< a >', '< href >', '< url >'],
+        correctAnswer: ['< a >']      
+    },
+    {
+        id: 6,
+        type: 'multiple',
+        question: 'Vilket av följande är INTE ett lag i La Liga?',
+        options: ['Valladolid', 'Las Palmas', 'Bologna', 'Real Betis'],
+        correctAnswer: ['Bologna']
+    },
+    {
+        id: 7,
+        type: 'checkbox',
+        question: 'Vilka av följande djur kan flyga?',
+        options: ['Fladdermus', 'Struts', 'Pingvin', 'Kolibri'],
+        correctAnswer: ['Fladdermus', 'Kolibri']
+    },
+    {
+        id: 8,
+        type: 'checkbox',
+        question: 'Vilka av följande år vann Barcelona Champions League?',
+        options: ['2006', '2009', '2010', '2015'],
+        correctAnswer: ['2006', '2009', '2015']
+    },
+    {
+        id: 9,
+        type: 'multiple',
+        question: 'Vilken spelare har gjort flest mål i La Ligas historia?',
+        options: ['Cristiano Ronaldo', 'Lionel Messi', 'Raúl', 'Karim Benzema'],
+        correctAnswer: ['Lionel Messi']
+    },
+    {
+        id: 10,
+        type: 'boolean',
+        question: 'Mount Everest är högre än 8000 meter.',
+        options: ['Sant', 'Falskt'],
+        correctAnswer: ['Sant']
     }
 ];
