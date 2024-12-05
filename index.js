729c5c6aebcbdde7efe2b2df017aa047aa396da4
Fadi-Jawhar <fadi.jawhr@hotmail.com>
2024-12-05 16:56:53 +0100

    add render all questions and next and prev button

--- index.js
+++ index.js
@@ -1,26 +1,46 @@
 import { questions } from './src/questions.js';
 
+
+const questionContainer = document.querySelector('#questionContainer');
+const prevButton = document.querySelector('#prevButton');
+const nextButton = document.querySelector('#nextButton');
+
+
 let currentIndex = 0;
 
 function renderQuestion(index) {
-    const question = questions[index];
-    questionContainer.innerHTML = `
-      <h2>${question.question}</h2>
-      <div>
-        ${question.options
-          .map(
-            (option, i) => `
-          <label>
-            <input type="radio" name="q${index}" value="${option}">
-            ${option}
-          </label>
-        `
-          )
-          .join('')}
-      </div>
-    `;
+  const question = questions[index];
+  questionContainer.innerHTML = `
+    <h2>${question.question}</h2>
+    <ul>
+      ${question.options.map(option => `<li>${option}</li>`).join('')}
+    </ul>
+  `;
+}
+
+function updateButtons() {
+  prevButton.disabled = currentIndex === 0;
+  nextButton.disabled = currentIndex === questions.length - 1;
+}
+
+
+document.body.append(prevButton, nextButton);
+
+prevButton.addEventListener('click', () => {
+  if (currentIndex > 0) {
+    currentIndex--;
+    renderQuestion(currentIndex);
+    updateButtons();
   }
+});
 
-
+nextButton.addEventListener('click', () => {
+  if (currentIndex < questions.length - 1) {
+    currentIndex++;
+    renderQuestion(currentIndex);
+    updateButtons();
+  }
+});
 
 renderQuestion(currentIndex);
+updateButtons();
