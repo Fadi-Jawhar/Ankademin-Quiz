0a876847982fcf52267952e87502ab18c5609f41
Fadi-Jawhar <fadi.jawhr@hotmail.com>
2024-12-05 16:15:24 +0100

    add html and render questions

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
 import { questions } from './src/questions.js';
 
-console.log(questions);
+let currentIndex = 0;
+
+function renderQuestion(index) {
+    const question = questions[index];
+    questionContainer.innerHTML = `
+      <h2>${question.question}</h2>
+      <div>
+        ${question.options
+          .map(
+            (option, i) => `
+          <label>
+            <input type="radio" name="q${index}" value="${option}">
+            ${option}
+          </label>
+        `
+          )
+          .join('')}
+      </div>
+    `;
+  }
+
+
+
+renderQuestion(currentIndex);
